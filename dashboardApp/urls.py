ef832b023ffec99cc5b74a445060c53c4c72a231
Kenneth-Marcel <50622082+Kenneth-Marcel@users.noreply.github.com>
2021-03-07 11:03:48 +0000

    Create urls.py

---
+++ dashboardApp/urls.py
@@ -0,0 +1,32 @@
+"""panel URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from interface.views import indexPage
+from interface.views import testPage 
+from interface.views import basePage
+from interface.views import chartPage
+
+
+
+urlpatterns = [
+	path('admin/', admin.site.urls),
+	path('', indexPage, name = 'index'),
+	path('test/', testPage, name='test'),
+    path('base/', basePage, name='base'),
+    path('chart/', chartPage , name='chart')
+     
+]
